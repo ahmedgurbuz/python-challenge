a997bb8df22bce4f65c9be466db85183e7e06177
ahmedgurbuz <ahmedgurbuz@gmail.com>
2019-09-04 22:48:22 -0500

    Read CSV file

---
+++ PyPoll/main.py
@@ -0,0 +1,12 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder
+
+path = r"C:\Users\AG\Documents\SMU COURSE\SMU-DAL-DATA-PT-08-2019-U-C\02-Homework\03-Python\Instructions\PyPoll\Resources\election_data.csv"
+
+# Read in the CSV file
+with open(path, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
