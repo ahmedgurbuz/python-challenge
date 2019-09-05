e019ffc1ac901cd546cd9ecd496049c59e310914
ahmedgurbuz <ahmedgurbuz@gmail.com>
2019-09-05 18:49:10 -0500

    Total Profit calculated

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,3 +10,32 @@ with open(path, 'r') as csvfile:
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader, None)
+
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Sep", "Oct", "Nov", "Dec"]
+    years = ["10","11","12","13", "14", "15", "16", "17"]
+
+    total_profit = 0
+
+    for row in csvreader:
+
+        #Assigning values to variables with descriptive names
+        date = row[0]
+        profit = row[1]
+        total_profit += int(profit)
+    
+    
+    
+    
+    
+    print("FINANCIAL ANALYSIS")
+    print("-"*20)
+    print(f"Total Months: {total_profit}")
+    print(f"Total: ${total_profit}")
+    print(f"Average  Change: ${total_profit}")
+    print(f"Greatest Increase in Profits: {total_profit}")
+    print(f"Greatest Decrease in Profits: {total_profit}")
+
+
+
