b14c0e61010c2859e528e1f0bd87b95a54c0a0bb
ahmedgurbuz <ahmedgurbuz@gmail.com>
2019-09-05 22:22:19 -0500

    Results table and writing to results.txt file coded

--- PyBank/main.py
+++ PyBank/main.py
@@ -35,17 +35,32 @@ with open(path, 'r') as budget_data:
         #Calculating total profit
         total_profit += int(profit)
     
-    
-    
+    total_months = (csvreader.line_num)-1
+    average_change = total_profit / total_months
     
     #Printing the final values
     print("FINANCIAL ANALYSIS")
     print("-"*20)
-    print(f"Total Months: {(csvreader.line_num)-1}")
+    print(f"Total Months: {total_months}")
     print(f"Total: ${total_profit}")
-    print(f"Average  Change: ${total_profit}")
+    print(f"Average Change: ${average_change}")
     print(f"Greatest Increase in Profits: {total_profit}")
     print(f"Greatest Decrease in Profits: {total_profit}")
 
+    # Open function to create and write on the file "Results.txt"  
+    file_object = open(r"C:\Users\AG\Documents\python-challenge\PyBank\Results.txt","w+")
 
+    #Assigning each lines in results table as variable
+    str1 = "FINANCIAL ANALYSIS"
+    str2 = str("-"*20)
+    str3 = str(f"Total Months: {total_months}")
+    str4 = str(f"Total: ${total_profit}")
+    str5 = str(f"Average Change: ${average_change}")
+    str6 = str(f"Greatest Increase in Profits: {total_profit}")
+    str7 = str(f"Greatest Decrease in Profits: {total_profit}")
 
+    #Uniting results table and writing line by line to text file
+    results = [str1 + "\n", str2 + "\n", str3 + "\n", str4 + "\n", str5 + "\n", str6 + "\n", str7 + "\n"] 
+    file_object.writelines(results)
+    #Closing text file
+    file_object.close()
