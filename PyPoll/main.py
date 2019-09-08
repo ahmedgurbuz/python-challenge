17c9f453a45ccddb615b5686da6bc44fa438cc96
ahmedgurbuz <ahmedgurbuz@gmail.com>
2019-09-07 23:58:35 -0500

    Codes are finalized. Final results are achieved

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,9 +2,13 @@ import os
 import csv
 
 # Path to collect data from the Resources folder
-
 path = r"C:\Users\AG\Documents\SMU COURSE\SMU-DAL-DATA-PT-08-2019-U-C\02-Homework\03-Python\Instructions\PyPoll\Resources\election_data.csv"
 
+
+#Creating empty lists to be filled later
+voter_id = []
+county = []
+candidate = []
 
 # Read in the CSV file
 with open(path, 'r') as election_data:
@@ -17,42 +21,71 @@ with open(path, 'r') as election_data:
 
     #Looping through csv file with our handle
     for row in csvreader:
+        
+        #Reading and adding every row in second column (profit/loss column) to profit list
+        voter_id.append(int(row[0]))
 
-        #Assigning values to variables with descriptive names
-        voter_id = row[0]
-        county = row[1]
-        candidate = row[2]
+        #Reading and adding every row in first column (months column) to months list
+        county.append(str(row[1]))
 
-        #Calculating total votes (total rows of data)
-        total_votes = (csvreader.line_num)-1
+        candidate.append(str(row[2]))
+        
     
+    total_votes = len(voter_id)
 
-   # Open function to create and write on the file "Results.txt"  
+
+    votes_dict = {}
+
+    for contestant in candidate:
+        if contestant in votes_dict:
+            votes_dict[contestant] += 1
+        else:
+            votes_dict[contestant] = 1
+
+    
+    candidates_sorted = sorted(
+        votes_dict.items(),
+        key=lambda contestant: contestant[1], 
+        reverse = True
+    )
+
+
+    percentages = []
+
+    for value in votes_dict.values():
+        percent = (value/total_votes)*100
+        percentages.append(percent)
+
+
+    print("Election Results")
+    print("-"*20)
+    print(f"Total Votes: {total_votes}")
+    print("-"*20)
+    print(f"{candidates_sorted[0]}: {round(percentages[0], 5)}%")
+    print(f"{candidates_sorted[1]}: {round(percentages[1], 5)}%")
+    print(f"{candidates_sorted[2]}: {round(percentages[2], 5)}%")
+    print(f"{candidates_sorted[3]}: {round(percentages[3], 5)}%")
+    print("-"*20)
+    print(f"Winner: {candidates_sorted[0]}")
+    print("-"*20)
+
+
+
+    # Open function to create and write on the file "Results.txt"  
     file_object = open(r"C:\Users\AG\Documents\python-challenge\PyPoll\Results.txt","w+")
 
-    #Assigning each lines in results table as variable
+    #Assigning each lines in results table as variable 
     str1 = "Election Results"
     str2 = str("-"*20)
     str3 = str(f"Total Votes: {total_votes}")
-    str4 = str(f"Khan: {total_votes}")
-    str5 = str(f"Correy: {total_votes}")
-    str6 = str(f"O'Tooley: {total_votes}")
-    str7 = str(f"Winner: {total_votes}")
-
-    #Printing the final values
-    print(str1)
-    print(str2)
-    print(str3)
-    print(str2)
-    print(str4)
-    print(str5)
-    print(str6)
-    print(str2)
-    print(str7)
-    print(str2)
+    str4 = str(f"{candidates_sorted[0]}: {round(percentages[0], 5)}%")
+    str5 = str(f"{candidates_sorted[1]}: {round(percentages[1], 5)}%")
+    str6 = str(f"{candidates_sorted[2]}: {round(percentages[2], 5)}%")
+    str7 = str(f"{candidates_sorted[3]}: {round(percentages[3], 5)}%")
+    str8 = str(f"Winner: {candidates_sorted[0]}")
 
     #Uniting results table and writing line by line to text file
-    results = [ str1 + "\n", str2 + "\n", str3 + "\n", str2 + "\n", str4 + "\n", str5 + "\n", str6 + "\n", str2 + "\n", str7 + "\n", str2] 
+    results = [str1 + "\n", str2 + "\n", str3 + "\n", str2 + "\n", str4 + "\n", str5 + "\n", str6 + "\n", str7 + "\n", str2 + "\n", str8 + "\n", str2 + "\n"]
     file_object.writelines(results)
     #Closing text file
     file_object.close()
